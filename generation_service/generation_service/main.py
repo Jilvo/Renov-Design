cd3fadaf8f1ae11ce7a60acb7ca121d0277021d3
Jilvo <jmillot41@gmail.com>
2024-08-11 18:53:35 +0200

    start generation_service && add stability-ia

--- generation_service/generation_service/main.py
+++ generation_service/generation_service/main.py
@@ -1,15 +1,44 @@
-from typing import Union
+from flask import Flask, request
 
-from fastapi import FastAPI
-
-app = FastAPI()
+app = Flask(__name__)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Generation Service successfully started!"}
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.route("/generate", methods=["POST"])
+def generate_image():
+    data = request.get_json()
+    prompt = data.get("prompt", "")
+    print(data)
+    if not prompt or prompt == "":
+        return {"code": 400, "message": "Prompt is empty!"}
+    try:
+        from diffusers import AutoPipelineForText2Image
+        import torch
+        import uuid
+
+        id_of_prompt = str(uuid.uuid4().hex)
+        print(f"ID of prompt: {id_of_prompt}")
+        pipe = AutoPipelineForText2Image.from_pretrained(
+            "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16"
+        )
+        pipe.to("cuda")
+
+        image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+
+        image.save(f"{id_of_prompt}.png")
+        print("Image generated successfully!")
+        return {
+            "code": 200,
+            "id": id_of_prompt,
+            "message": "Image generated successfully!",
+        }
+    except Exception as e:
+        return {"code": 500, "message": "Error in generating image!", "error": str(e)}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
