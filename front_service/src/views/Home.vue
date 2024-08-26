6cba082aa4322f35fa44dbcd539c2d1ea76e5e11
Jilvo <jmillot41@gmail.com>
2024-08-26 22:51:06 +0200

    create header && add tailwinds

---
+++ front_service/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header />
+  <div>Redesignez votre maison</div>
+</template>
+
+<script>
+import Header from '../components/Header/Header.vue'
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Header
+  }
+}
+</script>
